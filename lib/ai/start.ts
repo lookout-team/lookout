232ec13468ae0863ca143d3a192dbdb73bed44e9
Wasim Sandhu <sandhuwasim@gmail.com>
2024-04-14 00:25:28 -0700

    init: Track /lib directories

---
+++ lib/ai/start.ts
@@ -0,0 +1,4 @@
+// To get started, run `npm install` and then `npm run dev`.
+// This is just a blank file for adding the /lib/ai directory to git.
+// Feel free to delete it and create your own.
+// - @wasimsandhu
