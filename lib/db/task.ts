f3f5c61aabd174565638cea9d383649efb195a04
joelhenningsen <ravajoely@gmail.com>
2024-05-11 18:09:16 -0700

    Refactor to accept id as object

--- lib/db/task.ts
+++ lib/db/task.ts
@@ -94,10 +94,10 @@ export async function updateTask(params: Partial<Task>): Promise<Task> {
  * @param {number} id - Task ID
  * @returns {Promise<Task>} - The deleted task
  */
-export async function deleteTask(id: number): Promise<Task> {
+export async function deleteTask(params: Partial<Task>): Promise<Task> {
   const task = await prisma.task.delete({
     where: {
-      id: id,
+      id: params.id,
     },
   });
   return task;
