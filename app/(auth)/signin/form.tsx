a8d584cd1900ceac0802b221baaedb8bd434aba8
Wasim Sandhu <sandhuwasim@gmail.com>
2024-05-19 13:22:10 -0700

    fix: Remove unused import

--- app/(auth)/signin/form.tsx
+++ app/(auth)/signin/form.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button, Card, CardBody, Input } from "@nextui-org/react";
-import { useState } from "react";
 
 interface Props {
   action: (form: FormData) => Promise<void>;
